ea4de47ba3a267452862d636ac340dddf4e60d2b
ohernandez0307 <123129470+ohernandez0307@users.noreply.github.com>
2023-02-03 15:38:59 -0600

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,7 @@
+fun main() {
+var name: String
+name = "Oscar Hernandez"
+val age = 25
+var annualPay: Double = 100000.00
+println("My name is $name, my age is $age and I hope to earn $$annualPay per year.")    
+}
