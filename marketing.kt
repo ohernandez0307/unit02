246461dee11692deade301434b21c7fe79699cea
ohernandez0307 <123129470+ohernandez0307@users.noreply.github.com>
2023-02-03 14:57:08 -0600

    Introvert vs Extrovert

---
+++ marketing.kt
@@ -0,0 +1,10 @@
+fun main() {
+    var friendsCount = readln()
+    var personlity = friendsCount =<500
+    if (personality = true){
+        println("Buy Cat Food At 20% Off.")
+}
+else {
+    println("Adopt a Dog Today!")
+}
+}
