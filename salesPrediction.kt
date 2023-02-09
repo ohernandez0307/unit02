80e92146b4a0f098aae22e20918d2832cb464148
ohernandez0307 <123129470+ohernandez0307@users.noreply.github.com>
2023-02-09 17:36:31 -0600

    Sales prediction

---
+++ salesPrediction.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val companySales = 4600000
+    val eastCoastSales = .62
+
+    var salesDivision = (companySales * eastCoastSales)
+
+    println("The East Coast Company's sales division will generate about $$salesDivision in revenue for the company if East Coast reports $4.6 million in sales this year")
+
+
+}
