f151eeec70b95dedcf91082394480bafa9fed453
ohernandez0307 <123129470+ohernandez0307@users.noreply.github.com>
2023-02-12 01:07:40 -0600

    Add files via upload

---
+++ star.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    println("   *")
+    println("  ***")
+    println(" *****")
+    println("*******")
+    println(" *****")
+    println("  ***")
+    println("   *")
+
+
+}
+
